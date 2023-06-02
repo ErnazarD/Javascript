b8caedc8ccec1bcdee05fd1eb0e6b1083b87bee8
ErnazarD <74822221+ErnazarD@users.noreply.github.com>
2023-06-03 01:30:27 +0900

    message

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-console.log("testing");
-console.log("hellloo");
+let apiQuotes = [];
+
+// show new quotes 
+function newQuote(){
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// get Quotes from API
+async function getQuotes(){
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const responce = await fetch(apiUrl);
+        apiQuotes = await responce.json();
+        newQuote();
+    } catch (error) {
+        // Catch the error here
+    }
+}
+
+// On load
+getQuotes();
