42c49a140bb4ba50017dfc93614acc528dd8eeab
ErnazarD <74822221+ErnazarD@users.noreply.github.com>
2023-06-04 00:02:41 +0900

    final commit for (Quote Generator )

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
+const quoteConteiner = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+function loading(){
+    loader.hidden = false;
+    quoteConteiner.hidden = true;
+}
+
+// hide loader
+function complete(){
+    quoteConteiner.hidden = false;
+    loader.hidden =true;
+}
 
 // show new quotes 
 function newQuote(){
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    // Set the quote, hide Loader
+    quoteText.textContent = quote.text;
+    complete();
+
+
+    quoteText.textContent = quote.text;
+
 }
 
 // get Quotes from API
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const responce = await fetch(apiUrl);
@@ -17,6 +53,16 @@ async function getQuotes(){
         // Catch the error here
     }
 }
+// Tweet function 
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, `_blank`);
+}
+
+// event listener
+
+newQuoteBtn>addEventListener('click', newQuote);
+twitterBtn.addEventListener(`click`, tweetQuote);
 
 // On load
 getQuotes();
